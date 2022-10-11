d2e589be0ac54cafce77743e1a2168c2bab4d6b3
Prayansh <parmarprayansh2@gmail.com>
2022-10-11 20:17:38 +0530

    commit

--- index.php
+++ index.php
@@ -1,50 +1,78 @@
-<?php 
-require('UserInfo.php');
+<?php
+
+function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
+    $output = NULL;
+    if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
+        $ip = $_SERVER["REMOTE_ADDR"];
+        if ($deep_detect) {
+            if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+    }
+
+    $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
+    $support    = array("country", "countrycode", "state", "region", "city", "location", "address");
+    $continents = array(
+        "AF" => "Africa",
+        "AN" => "Antarctica",
+        "AS" => "Asia",
+        "EU" => "Europe",
+        "OC" => "Australia (Oceania)",
+        "NA" => "North America",
+        "SA" => "South America"
+    );
+    if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) 
+	{
+        $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+        if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
+            switch ($purpose) {
+                case "location":
+                    $output = array(
+                        "city"           => @$ipdat->geoplugin_city,
+                        "state"          => @$ipdat->geoplugin_regionName,
+                        "country"        => @$ipdat->geoplugin_countryName,
+                        "country_code"   => @$ipdat->geoplugin_countryCode,
+                        "continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
+                        "continent_code" => @$ipdat->geoplugin_continentCode
+                    );
+                    break;
+                case "address":
+                    $address = array($ipdat->geoplugin_countryName);
+                    if (@strlen($ipdat->geoplugin_regionName) >= 1)
+                        $address[] = $ipdat->geoplugin_regionName;
+                    if (@strlen($ipdat->geoplugin_city) >= 1)
+                        $address[] = $ipdat->geoplugin_city;
+                    $output = implode(", ", array_reverse($address));
+                    break;
+                case "city":
+                    $output = @$ipdat->geoplugin_city;
+                    break;
+                case "state":
+                    $output = @$ipdat->geoplugin_regionName;
+                    break;
+                case "region":
+                    $output = @$ipdat->geoplugin_regionName;
+                    break;
+                case "country":
+                    $output = @$ipdat->geoplugin_countryName;
+                    break;
+                case "countrycode":
+                    $output = @$ipdat->geoplugin_countryCode;
+                    break;
+            }
+        }
+	}	
+
+}
+
+echo ip_info("Visitor", "Country"); // India
+echo ip_info("Visitor", "Country Code"); // IN
+echo ip_info("Visitor", "State"); // Andhra Pradesh
+echo ip_info("Visitor", "City"); // Proddatur
+echo ip_info("Visitor", "Address"); // Proddatur, Andhra Pradesh, India
+
+print_r(ip_info("Visitor", "Location")); // Array ( [city] => Proddatur [state] => Andhra Pradesh [country] => India [country_code] => IN [continent] => Asia [continent_code] => AS )
+
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>UserInfo Demo</title>
-	<style>
-table {
-	margin-top: 20px;
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
-
-td, th {
-    border: 1px solid #dddddd;
-    text-align: center;
-    padding: 8px;
-}
-
-tr:nth-child(even) {
-    background-color: #dddddd;
-}
-h2{font-family: sans-serif,'Helvetica';}
-</style>
-
-</head>
-<body>
-<center><h2>UserInfo demo</h2></center>
-
-
-	<table>
-		<tr>
-			<th>Ip</th>
-			<th>Device</th>
-			<th>Os</th>
-			<th>Browser</th>
-		</tr>
-		<tr>
-			<td><?= echo UserInfo::get_ip();?></td>
-			<td><?= echo UserInfo::get_device();?></td>
-			<td><?= echo UserInfo::get_os();?></td>
-			<td><?= echo UserInfo::get_browser();?></td>
-		</tr>
-	</table>
-
-</body>
-</html>
